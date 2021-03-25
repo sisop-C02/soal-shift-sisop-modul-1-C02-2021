775f954d1052fe27a9bf3fe54482f55430d090b3
maroqijalil <jalilpro31@gmail.com>
2021-03-25 12:23:11 +0700

    simplify solution of soal1 n soal2

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,81 +1,65 @@
 #!/bin/bash
 
 # Sangat bisa untuk disederhanakan lagi
-# versi 1
+# versi 2
 
-# 1.d
 declare -A messages
-index_result=()
-
-input_file=syslog.log
-output_file=error_message.csv
-
-while read lines
-do
-    line=${lines##*ERROR }
-    message=${line% (*}
-    if [[ ! "${messages[@]}" = *${message}* ]]
-    then
-        index=$(grep -wc "${message}" syslog.log)
-        messages+=([$index]="$message")
-        index_result+=($index)
-    fi
-done < <(sed -n '/ERROR/p' $input_file)
-
-index_result=($(echo ${index_result[*]}| tr " " "\n" | sort -n))
-
-echo "Error,Count" >> $output_file
-for (( i = `expr ${#index_result[@]} - 1`; i >= 0; i--))
-do
-    echo "${messages[${index_result[$i]}]},${index_result[$i]}" >> $output_file
-done
-
-# 1.e
 declare -A users_info
 declare -A users_error
+
+index_result=()
 users=()
 
 input_file=syslog.log
-output_file=user_statistic.csv
+output_file_1=error_message.csv
+output_file_2=user_statistic.csv
 
-getUserLog()
+checkLinesForUser()
 {
-    local name=$1
-    local error_num=0
-    local info_num=0
-
-    while read lines
-    do
-        if [[ $lines = *ERROR* ]]
-        then
-            error_num=`expr $error_num + 1`
-        fi
-
-        if [[ $lines = *INFO* ]]
-        then
-            info_num=`expr $info_num + 1`
-        fi
-    done < <(grep -i "($name)" $input_file)
-
-    users_info+=(["$name"]=$info_num)
-    users_error+=(["$name"]=$error_num)
-}
-
-while read lines
-do
-    line=${lines##*(}
+    line=${1##*(}
     user=${line%)*}
     if [[ ! "${users[@]}" = *$user* ]]
     then
         users+=("$user")
-        getUserLog $user
+        user_info=$(grep -i "($user)" $input_file | grep -wc "INFO")
+        user_error=$(grep -i "($user)" $input_file | grep -wc "ERROR")
+        users_info+=(["$user"]=$user_info)
+        users_error+=(["$user"]=$user_error)
     fi
+}
+
+checkLinesForError()
+{
+    if [[ "$1" = *ERROR* ]]
+    then
+        line=${1##*ERROR }
+        message=${line% (*}
+        if [[ ! "${messages[@]}" = *${message}* ]]
+        then
+            index=$(grep -wc "$message" $input_file)
+            messages+=([$index]="$message")
+            index_result+=($index)
+        fi
+    fi
+}
+
+while read lines
+do
+    checkLinesForUser "$lines"
+    checkLinesForError "$lines"
 done < $input_file
 
+index_result=($(echo ${index_result[*]}| tr " " "\n" | sort -n))
 users=($(echo ${users[*]}| tr " " "\n" | sort -n))
 
-echo "Username,INFO,ERROR" >> $output_file
+echo "Error,Count" >> $output_file_1
+for (( i = `expr ${#index_result[@]} - 1`; i >= 0; i--))
+do
+    echo "${messages[${index_result[$i]}]},${index_result[$i]}" >> $output_file_1
+done
+
+echo "Username,INFO,ERROR" >> $output_file_2
 for user in ${users[@]}
 do
-    echo "${user},${users_info[$user]},${users_error[$user]}" >> $output_file
+    echo "${user},${users_info[$user]},${users_error[$user]}" >> $output_file_2
 done
--- soal2/soal2_generate_laporan_ihir_shisop.sh
+++ soal2/soal2_generate_laporan_ihir_shisop.sh
@@ -1,9 +1,106 @@
 #!/bin/bash
 
-awk -F '[\t]' -f cari_max_profit.awk Laporan-TokoShiSop.tsv >> hasil.txt; 
-echo "" >> hasil.txt;
-awk -F '[\t]' -f cari_customer_albuquerque.awk Laporan-TokoShiSop.tsv >> hasil.txt;
-echo "" >> hasil.txt;
-awk -F '[\t]' -f cari_min_transaksi.awk Laporan-TokoShiSop.tsv >> hasil.txt;
-echo "" >> hasil.txt;
-awk -F '[\t]' -f cari_min_keuntungan.awk Laporan-TokoShiSop.tsv >> hasil.txt;
+output_file=hasil.txt
+
+awk -F '[\t]' '
+BEGIN   {
+    max_profit = 0;
+    max_id = 0;
+}
+
+{
+    if (n) {
+        profit=($21/($18-$21))*100;
+        if (profit > max_profit) {
+            max_profit=profit;
+        }
+        result[$1]=profit;
+        transaction_ids[$1]=$2;
+    }
+
+    ++n;
+}
+
+END {
+    for (i in result) {
+        if (result[i] == max_profit) {
+            if (i > max_id) {
+                max_id = i;
+                transaction_id=transaction_ids[i];
+            }
+        }
+    }
+    print "Transaksi terakhir dengan profit percentage terbesar yaitu", transaction_id,
+        "dengan prosentase", max_profit"%";
+}
+' Laporan-TokoShiSop.tsv >> $output_file;
+echo "" >> $output_file;
+awk -F '[\t]' '
+BEGIN   {
+    print "Daftar nama customer di Albuquerque pada tahun 2017 antara lain:";
+}
+
+/Albuquerque/ {
+    if (match($2, "2017")) {
+        result[$7]=1;
+    }
+}
+
+END {
+    for (i in result) {
+        print i;
+    }
+}
+' Laporan-TokoShiSop.tsv >> $output_file;
+echo "" >> $output_file;
+awk -F '[\t]' '
+BEGIN   {
+    min_transaction = 999999;
+}
+
+{
+    if (n) {
+        segment_transaction[$8] = segment_transaction[$8] + $19;
+    }
+
+    ++n;
+}
+
+END {
+    for (i in segment_transaction) {
+        if (segment_transaction[i] < min_transaction) {
+            segment = i;
+            min_transaction = segment_transaction[i];
+        }
+    }
+
+    print "Tipe segmen customer yang penjualannya paling sedikit adalah",
+        segment, "dengan", min_transaction, "transaksi";
+}
+' Laporan-TokoShiSop.tsv >> $output_file;
+echo "" >> $output_file;
+awk -F '[\t]' '
+BEGIN   {
+    min_profit = 999999.0;
+}
+
+{
+    if (n) {
+        region_profit[$13]=region_profit[$13]+$21;
+    }
+
+    ++n;
+}
+
+END {
+    for (i in region_profit) {
+        if (region_profit[i] < min_profit) {
+            region = i;
+            min_profit = region_profit[i];
+        }
+    }
+
+    print "Wilayah bagian (region) yang memiliki total keuntungan (profit) yang paling",
+        "sedikit adalah", region, "dengan total keuntungan", min_profit;
+}
+' Laporan-TokoShiSop.tsv >> $output_file;
