6715c81c63bbf521159446002f69ab810dfa79c1
Muhammad Zhafran <frans.zhafran@gmail.com>
2021-03-25 16:10:49 +0700

    soal3c udah kayanya

---
+++ soal3/soal3c.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+i=$(date "+%-e")
+i=$((i % 2))
+
+kucing_url="https://loremflickr.com/320/240/kitten"
+kelinci_url="https://loremflickr.com/320/240/bunny"
+
+kucing_folder="Kucing_"$(date "+%d-%m-%Y")
+kelinci_folder="Kucing_"$(date "+%d-%m-%Y")
+
+mkdir "$kucing_folder"
+mkdir "$kelinci_folder"
+
+if [ "$i" -eq "1" ]
+then
+	# kelinci
+	wget "$kelinci_url"
+	mv 'bunny' "$kelinci_folder"/	
+else
+	# kucing
+	wget "$kucing_url"
+	mv 'kitten' "$kucing_folder"/
+fi
