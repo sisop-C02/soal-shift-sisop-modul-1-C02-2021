8e39bb5f69675a4eb6c25429eaf4522f9433c66d
maroqijalil <jalilpro31@gmail.com>
2021-03-27 21:44:48 +0700

    make an option on 3d

--- soal3/soal3d.sh
+++ soal3/soal3d.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# zip the folders
 password=$(date "+%m%d%Y")
 zip -P $password -r Koleksi.zip */
 
+# delete the folders
 rm -R -- */
+
+# opsi 1
+# password=$(date "+%m%d%Y")
+# zip -P $password -r Koleksi.zip Kucing_* Kelinci_*
+# rm -R -- Kucing_* Kelinci_*
